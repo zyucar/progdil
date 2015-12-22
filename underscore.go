4b7e5ea901ec0736139d6423209e3c7de18d06c2
zeynep, uçar <zeynep.ucar016@gmail.com>
2015-12-22 02:41:05 +0200

    underscore

---
+++ underscore.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	 "fmt"
+	"strings"
+)
+
+func ReplaceUnderscoreWithScape(str string) string {
+        list := strings.Split(str, "")
+
+        for i := 1 ; i < len(list)-1 ; i++ {
+		if list[i] == "_" {
+			list[i] = " "
+                }
+        }
+        return(strings.Join(list, ""))
+}
+    
+func main() {
+	var word string
+	fmt.Println("Bir kelime giriniz.")
+	fmt.Scanf("%s", &word)
+	fmt.Println(ReplaceUnderscoreWithScape(word))
+}
+
